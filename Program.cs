e23646665025fc096749fe4d9efd2a581d89fec8
PhilippKorkunov <filippkorkunov.1920@gmail.com>
2023-04-05 14:01:28 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using DotNetTestApp;
+using System;
+
+string[] A = new string[] { "unlock.microvirus.md ", "visitwar.com", "visitwar.de", "fruonline.co.uk", "australia.open.com", "credit.card.us" };
+string[] B = new string[] { "microvirus.md", "visitwar.de", "piratebay.co.uk", "list.stolen.credit.card.us" };
+
+var s = Solution.solution(A, B);
+
+Console.WriteLine(String.Join(" ", s));
---
+++ Solution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace DotNetTestApp
+{
+    public class Solution
+    {
+        public static int[] solution(string[] A, string[] B)
+        {
+            string pattern = String.Join("|", B.Select(x => $@"\w*{x}"));
+            Console.WriteLine(pattern);
+
+            Regex regex = new(pattern, RegexOptions.Compiled);
+
+            List<int> answer = new();
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (!regex.IsMatch(A[i]))
+                {
+                    answer.Add(i);
+                }
+            }
+
+            return answer.ToArray();
+        }
+
+    }
+}
